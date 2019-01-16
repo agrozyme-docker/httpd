4c257f7625ce93ac2e1a258b305d318562e0242b
agrozyme <agrozyme@gmail.com>
2019-01-17 04:57:26 +0800

    update

--- rootfs/usr/local/bin/docker-build.lua
+++ rootfs/usr/local/bin/docker-build.lua
@@ -1,22 +1,20 @@
 #!/usr/bin/lua
 local core = require("docker-core")
 
-local function update_http2()
-  core.append_file("/etc/apache2/conf.d/http2.conf", "Protocols h2 h2c http/1.1 \n")
+local function update_http2(target, items)
+  core.append_file(target, "Protocols h2 h2c http/1.1 \n")
 end
 
-local function update_proxy()
-  local pcre = require("rex_pcre")
-  local file = "/etc/apache2/conf.d/proxy.conf"
-  local text = core.read_file(file)
+local function update_proxy(target, items)
+  local pcre = items.pcre
+  local text = core.read_file(target)
   text = pcre.gsub(text, [[^[#\s]*(LoadModule[\s]+.*)$]], "# %1", nil, "im")
-  core.write_file(file, text)
+  core.write_file(target, text)
 end
 
-local function update_httpd()
-  local pcre = require("rex_pcre")
-  local file = "/etc/apache2/httpd.conf"
-  local text = core.read_file(file)
+local function update_httpd(target, items)
+  local pcre = items.pcre
+  local text = core.read_file(target)
   text = pcre.gsub(text, [[^#LoadModule[\s]+negotiation_module[\s]+.*$]], "", nil, "im")
   text = pcre.gsub(text, [[^[#\s]*(LoadModule[\s]+.*)$]], "# %1", nil, "im")
   text =
@@ -31,18 +29,18 @@ local function update_httpd()
   text = pcre.gsub(text, [[^[#\s]*(ServerName)[\s]+[.:\w]+$]], "%1 127.0.0.1", nil, "im")
   text = pcre.gsub(text, [[([\s]+"/var/www/)localhost/htdocs(/?")]], "%1html%2", nil, "im")
   text = pcre.gsub(text, [[([\s]+"/var/www/)localhost/(cgi-bin/?")]], "%1%2", nil, "im")
-  core.write_file(file, text)
-  core.append_file(file, "IncludeOptional /usr/local/etc/apache2/*.conf \n")
+  core.write_file(target, text)
+  core.append_file(target, "IncludeOptional /usr/local/etc/apache2/*.conf \n")
 end
 
 local function replace_setting()
-  if (core.has_modules("rex_pcre")) then
-    update_http2()
-    update_proxy()
-    update_httpd()
-  else
-    core.replace_files("/etc/apache2/httpd.conf", "/etc/apache2/conf.d/http2.conf", "/etc/apache2/conf.d/proxy.conf")
-  end
+  local requires = {pcre = "rex_pcre"}
+  local updates = {
+    ["/etc/apache2/httpd.conf"] = update_httpd,
+    ["/etc/apache2/conf.d/http2.conf"] = update_http2,
+    ["/etc/apache2/conf.d/proxy.conf"] = update_proxy
+  }
+  core.replace_files(requires, updates)
 end
 
 local function main()
