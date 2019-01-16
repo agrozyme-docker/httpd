7e48e0435d6401319dd90402d545e9259955ea9d
agrozyme <agrozyme@gmail.com>
2019-01-16 22:12:36 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ COPY rootfs /
 RUN set +e -uxo pipefail && chmod +x /usr/local/bin/* && /usr/local/bin/docker-build.lua
 WORKDIR /var/www
 EXPOSE 80
-CMD ["/usr/local/bin/httpd.lua"]
+CMD ["/usr/local/bin/docker-run.lua"]
--- rootfs/usr/local/bin/docker-run.lua
+++ rootfs/usr/local/bin/docker-run.lua
@@ -4,7 +4,7 @@ local function main()
   local core = require("docker-core")
   core.update_user()
   core.clear_path("/run/apache2")
-  core.run("chown -R core:core /var/www/cgi-bin /var/www/html")
+  core.chown("/var/www/cgi-bin", "/var/www/html")
   core.run("httpd -DFOREGROUND")
 end
 
